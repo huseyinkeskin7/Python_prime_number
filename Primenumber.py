e69092825f0323f55d0078f685ca3f893efc5c29
Hüseyin Berk Keskin <119900674+huseyinkeskin7@users.noreply.github.com>
2023-01-14 03:16:18 +0300

    Add files via upload

---
+++ Primenumber.py
@@ -0,0 +1,9 @@
+def isprime(num):
+    if num> 1:  
+        for n in range(2,num):  
+            if (num % n) == 0:  
+                return False
+        return True
+    else:
+        return False
+print(isprime(int(input("Please enter your number: "))))
